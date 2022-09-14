d89f954df2357a3347c01422f2a35d5c9c12ff58
rubayetseason <season.ss2001@gmail.com>
2022-09-15 03:35:01 +0600

    done

--- history.js
+++ history.js
@@ -3,7 +3,7 @@ const histories = document.getElementById("histories");
 function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
-
+  
   newRow.innerHTML = `
   <h3>${questionText}</h3>
   <div>
@@ -11,7 +11,7 @@ function addHistory(questionText, timeTaken, errorCount) {
   <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
   </div>
   `;
-
+  
   histories.appendChild(newRow);
 
   let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
@@ -22,16 +22,16 @@ function addHistory(questionText, timeTaken, errorCount) {
 }
 
 function displayHistory() {
-  histories.innerHTML = "";
+  histories.innerHTML = ``;
   const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
-
+  
   previousTests.forEach((test) => {
     const newRow = document.createElement("div");
     newRow.classList.add("card");
-
+    
     newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+    <h3>${test.questionText}</h3>
+    <p>You took: <span class="bold"> ${test.timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
